90b2fb0a80cfbec9d19e7786096a4794df0f9c9b
chuanyue <2399147152@cy.com>
2023-01-11 23:15:05 +0800

    autojs

---
+++ auto.js/README.md
@@ -0,0 +1,3 @@
+[Auto.js文档](https://hyb1996.github.io/AutoJs-Docs/#/)  
+[Auto.js pro文档](https://pro.autojs.org/docs/zh/v8/)  
+[Autox.js文档](http://doc.autoxjs.com/#/)
---
+++ auto.js/demo.js
@@ -0,0 +1,14 @@
+toast('2399147152')
+
+// home();
+// sleep(1500);
+// click('设置');
+
+// app.launch("com.tencent.mobileqq");
+
+// device.wakeUp();
+
+device.keepScreenOn();
+
+// 振动
+device.vibrate(3000);
